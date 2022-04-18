96dbdc6632d02da69f9af9dca2485b56de87d6cc
Alexchap88 <100308590+Alexchap88@users.noreply.github.com>
2022-04-18 13:53:21 +0300

    Delete bot directory

--- bot/Creeds.py
+++
@@ -1,7 +0,0 @@
-telegramid = '937457889'
-
-Host = "ec3-3-230-966-20.compute-1.amazonaws.com"
-Database = "dfseestca7vrp6lo"
-User = "xwxsefdsgdgdsxynqf"
-Port = 5432
-Password ="fdhdgjdseeehrrsddgsdg9f"
